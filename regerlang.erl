bd92593091bdfabe1250e2b86c40543f9cd82ab5
szalaytb <57213660+szalaytb@users.noreply.github.com>
2020-05-10 11:27:46 +0200

    works on windows, mac, nix, added preview

--- regerlang.erl
+++ regerlang.erl
@@ -18,7 +18,7 @@ main(_) ->
     
 start() ->
      Wx=wx:new(),
-     F=wxFrame:new(Wx, ?wxID_ANY, "Regerlang, parsing files with ease", [{size, {800,500}}]),
+     F=wxFrame:new(Wx, ?wxID_ANY, "Regerlang, parsing files with ease", [{size, {1100,600}}]),
      wxFrame:show(F),
      MenuBar = wxMenuBar:new(),
      Help = wxMenu:new(),
@@ -31,20 +31,34 @@ start() ->
      wxFrame:createStatusBar(F),
      wxFrame:connect(F, command_menu_selected),
      io:fwrite("\n\nQuicly quit with command + Q on MacOS or alt + F4 on Windows and kill erlang with the corresponding command from linux terminal, for now...\n"),
-     Button_regex = wxButton:new(F, ?wxID_ANY, [{label, "Do the regex scan"}, {pos,{0, 214}}, {size, {150, 50}}]),
+     Button_regex = wxButton:new(F, ?wxID_ANY, [{label, "Regex to file"}, {pos,{0, 214}}, {size, {150, 50}}]),
      Button_file = wxButton:new(F, ?wxID_ANY, [{label, "ScanTheFile"}, {pos,{0, 314}}, {size, {150, 50}}]),
+     Button_preview = wxButton:new(F, ?wxID_ANY, [{label, "Preview result"}, {pos, {0, 414}}, {size, {150,50}}]),
      wxStaticText:new(F, ?wxID_ANY, "Link the file you wish to scan", [{pos, {100,60}}]),
      wxStaticText:new(F, ?wxID_ANY, "Type here your regular expression", [{pos, {160,140}}]),
      EventText = wxTextCtrl:new(F, 
 					  ?wxID_ANY, 
 					  [{style, ?wxTE_MULTILINE},{pos, {160,160}},{size, {400, 200}}
 					  ]),
+     EventText2 = wxTextCtrl:new(F, 
+					  ?wxID_ANY, 
+					  [{style, ?wxTE_MULTILINE},{pos, {600,160}},{size, {400, 200}}
+					  ]),
      Path = wxFilePickerCtrl:new(F,
 					  ?wxID_ANY,
 					  [{style, ?wxTE_MULTILINE},{pos, {100,80}}, {size, {100,50}}
 					  ]),
 	     
-     wxButton:connect(Button_regex, command_button_clicked, [{callback,
+     wxButton:connect(Button_preview, command_button_clicked, [{callback,
+	     fun(Evt, Obj) ->
+			   {_,_} = {Evt, Obj},
+			   {_, T} = file:read_file(wxFilePickerCtrl:getPath(Path)),
+			   Result = re:run(T, wxTextCtrl:getValue(EventText),[global,{capture,all,list}]),
+			   wxTextCtrl:writeText(EventText2, Result)
+			   end
+	     }]),
+
+    wxButton:connect(Button_regex, command_button_clicked, [{callback,
 	     fun(Evt, Obj) ->
 			   {_,_} = {Evt, Obj},
 			   {_, T} = file:read_file(wxFilePickerCtrl:getPath(Path)),
@@ -52,8 +66,7 @@ start() ->
 			   file:write_file("erlangTest", io_lib:fwrite("~p.\n", [Result]))
 			   %%wxTextCtrl:writeText(EventText2, T2)
 			   end
-	     }]),
-	     
+	     }]),	     
      wxButton:connect(Button_file, command_button_clicked, [{callback,
 	     fun(Evt, Obj) ->
 			   {_,_} = {Evt, Obj},
